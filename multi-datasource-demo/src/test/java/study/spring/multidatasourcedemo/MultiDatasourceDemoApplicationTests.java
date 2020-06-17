1ef936a76ed7793293150b585a4168e17feb2141
wuguokai <guokai.wu.work@gmail.com>
2020-06-17 22:32:27 +0800

    [ADD] 添加了多数据源配置的demo

---
+++ multi-datasource-demo/src/test/java/study/spring/multidatasourcedemo/MultiDatasourceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.multidatasourcedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDatasourceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
